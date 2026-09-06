2a8ff945aaaa5a8ad9ac84978b5b255e13ee467e
sakyasensingha742-hash <sakyasensingha742@gmail.com>
2026-09-06 20:13:04 +0530

    fix: connect drawing status events

--- src/drawing/DrawingWorkspace.tsx
+++ src/drawing/DrawingWorkspace.tsx
@@ -4,5 +4,5 @@ import DrawingCanvas from './DrawingCanvas';
 type Props={onStatus?:(message:string)=>void};
 export default function DrawingWorkspace({onStatus}:Props){
  const [size,setSize]=useState(6);
- return <div className="drawing-workspace"><div className="workspace-title">DRAWING & ILLUSTRATION</div><div className="drawing-toolbar"><label>Brush Size <input type="range" min="1" max="40" value={size} onChange={e=>setSize(Number(e.target.value))}/><b>{size}px</b></label><button onClick={()=>onStatus?.('Drawing layer ready')}>New Drawing Layer</button></div><DrawingCanvas brushSize={size}/></div>;
+ return <div className="drawing-workspace"><div className="workspace-title">DRAWING & ILLUSTRATION</div><div className="drawing-toolbar"><label>Brush Size <input type="range" min="1" max="40" value={size} onChange={e=>setSize(Number(e.target.value))}/><b>{size}px</b></label><button onClick={()=>onStatus?.('Drawing layer ready')}>New Drawing Layer</button></div><DrawingCanvas brushSize={size} onStatus={onStatus}/></div>;
 }
