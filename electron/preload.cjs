77357043d323a7a62e00881b03b28a1cc1036fe4
sakyasensingha742-hash <sakyasensingha742@gmail.com>
2026-09-06 21:04:57 +0530

    Add isolated desktop IPC bridge

---
+++ electron/preload.cjs
@@ -0,0 +1,16 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('mayaShadowDesktop', {
+  getAppInfo: () => ipcRenderer.invoke('studio:app-info'),
+  checkForUpdate: () => ipcRenderer.invoke('studio:check-update'),
+  downloadUpdate: () => ipcRenderer.invoke('studio:download-update'),
+  installUpdate: () => ipcRenderer.invoke('studio:install-update'),
+  saveProject: (payload) => ipcRenderer.invoke('studio:save-project', payload),
+  loadProject: () => ipcRenderer.invoke('studio:load-project'),
+  backupProject: (payload) => ipcRenderer.invoke('studio:backup-project', payload),
+  onUpdateStatus: (callback) => {
+    const listener = (_event, data) => callback(data);
+    ipcRenderer.on('studio:update-status', listener);
+    return () => ipcRenderer.removeListener('studio:update-status', listener);
+  }
+});
